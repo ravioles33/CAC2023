c0f726c80244c6fc1bf272b93fc58232ac0b14f0
ravioles33 <99939387+ravioles33@users.noreply.github.com>
2023-04-02 13:22:15 -0300

    Agrego h1 a la ruta vacía

--- primerDjango/publica/urls.py
+++ primerDjango/publica/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('hola_mundo', views.hola_mundo, name='hola'), #cuando reciba la ruta 'hola_mundo' 
 #{atributo1}, vaya a ejecutar el método de mi view 'hola_mundo' {atributo2}, 
 #y le agrego además un shortcut-apodo para estar ruta 'hola' {atributo3}
+    path('', views.index, name='inicio'), #la ruta vacía lleva acá
 ]
--- primerDjango/publica/views.py
+++ primerDjango/publica/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def hola_mundo(request):
     return HttpResponse('Hola mundo Django')
+
+def index(request):
+    return HttpResponse('<h1> DALE CHE 2/4/23 </h1>')
