3399ae961b8ae32a668f2e56d7a2efa5cdf3d285
ravioles33 <99939387+ravioles33@users.noreply.github.com>
2023-04-15 12:31:04 -0300

    agregado método saludar a views.py

--- primerDjango/publica/views.py
+++ primerDjango/publica/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+#Vistas: Crear métodos dentro de nuestro views.py para que la rutas puedan estar asociadas a un path específico y a un método que queremos que resuelva esa solicitud.
+
 # Create your views here.
 def hola_mundo(request):
     return HttpResponse('Hola mundo Django')
 
 def index(request):
     return HttpResponse('<h1> DALE CHE 2/4/23 </h1>')
+
+def saludar(request, nombre): #request es una instancia de HttpRequest
+    pass
